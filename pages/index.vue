acfcfbdce45a06b903ed667397615f40b7a7b81d
Sixpacks123 <aub.heurtault@gmail.com>
2021-10-20 09:38:21 +0200

    V.1

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        SavoirInutile
+      </h1>
+      <h2 class="subtile" v-on:click="update()">
+        {{phrase}}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      phrase : "Defautl"
+    }
+  },methods:{
+    update(){
+      axios.get('https://uselessfacts.jsph.pl/random.json?language=en').then(reponse => this.phrase  = reponse.data.text)
+
+    }
+  },
+  mounted(){
+    axios.get('https://uselessfacts.jsph.pl/random.json?language=en').then(reponse => this.phrase  = reponse.data.text)
+  }
+}
+</script>
+<style>
+  .container{
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title{
+    display: block;
+    font-weight: 300;
+    font-size: 42px;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links{
+    padding-top:15px;
+  }
+</style>
